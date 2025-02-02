0b112bef9de66a5981467d6fb6cdc1ea2b0c6ae0
Jake <jemoya1994@gmail.com>
2025-02-01 21:20:34 -0800

    adding all code to respective repo for now, just a start - Tailwind setup/ Fonts gathered/ settling on a color theme next

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{html,js,ts,jsx,tsx}',
+  ],
+  theme: {
+    fontFamily: {
+      'main': ['Noto Serif', 'serif'],
+      'secondary': ['Montserrat', 'serif']
+    },
+  },
+  plugins: [],
+}
+
