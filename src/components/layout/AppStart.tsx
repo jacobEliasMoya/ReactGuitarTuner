6496bc41c30989e6c09a108958f0a6f04439cf8b
Jake <jemoya1994@gmail.com>
2025-02-03 17:46:27 -0800

    added in minor onload animation :) just messing with tailwindcss/ extending its capbilites

--- src/components/layout/AppStart.tsx
+++ src/components/layout/AppStart.tsx
@@ -1,16 +1,24 @@
 import H1 from '../headers/H1'
 import MainButton from '../buttons/MainButton'
+import { useEffect, useState } from 'react'
 
 type Props = {
     additionalClasses:string | undefined,
-      onclick: (event:React.MouseEvent<HTMLButtonElement>)=>void,
+    onclick: (event:React.MouseEvent<HTMLButtonElement>)=>void,
   }
 
 const AppStart = (props:Props) => {
 
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    console.log('AppStart component is loaded')
+  },[isLoaded])
+  
+
   return (
-    <div className=" md:w-96 w-64 h-64 md:h-96 bg-white rotate-45 flex flex-col items-center justify-center  transition-all ease [border-radius:15%_90%_62%_41%_/_15%_41%_62%_90%] ">
-      <div className=' -rotate-45 text-center flex flex-col gap-4 '>
+    <div className=" animate-drop-n-rotate md:w-96 w-64 h-64 md:h-96 bg-white -rotate-[135deg] flex flex-col items-center justify-center  transition-all ease [border-radius:15%_90%_62%_41%_/_15%_41%_62%_90%] [box-shadow:_-15em_-15em_1em_-5em_#00000026]">
+      <div className=' rotate-[135deg] text-center flex flex-col gap-4 -translate-x-3 -translate-y-3 '>
           <H1 headerText={"Get Ready"} additionalClasses={'font-secodnary'} textIcon={undefined}/>
           <MainButton buttonText="Let's Tune" textIcon={undefined} additionalClasses={''} onclick={props.onclick}/> 
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,30 @@ export default {
       'main': ['Noto Serif', 'serif'],
       'secondary': ['Montserrat', 'serif']
     },
+
+    extend: {
+      animation: {
+        'drop-n-rotate': 'dropnrotate .35s ease-in-out  1 forwards',
+      },
+      keyframes: {
+        dropnrotate: {
+          '0%': { 
+            transform: 'rotate(-135deg) translateY(150%) translateX(150%) ',
+            boxShadow: '-25em -25em 1em -4em #00000026' 
+
+          },
+
+          '100%': { 
+            transform: 'rotate(-135deg) translateY(0%) translateX(0%)',
+            boxShadow: '0em 0em 0em 0em #00000026' 
+           },
+        },
+      },
+    },
+
   },
-  plugins: [],
+  plugins: [
+    require("@xpd/tailwind-3dtransforms"),
+  ],  
 }
 
