d5376d13095a4a0f1fc73507d8aa223dfa8716c2
Jake <jemoya1994@gmail.com>
2025-02-10 20:41:43 -0800

    added new component/ will introduce another slice of state with respective questions/ will add to json doc/ or a new one, based on the id ... thinking face lol .... sounded better that way

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import { useEffect, useState } from "react"
 import AppStart from "./components/layout/AppStart"
 import WhatsTuning from "./features/instrument/WhatsTuning";
 import {  Route, Routes, useNavigate } from "react-router";
+import TunerArea from "./features/interests/TunerArea";
+import { useAppSelector } from "./hooks/hooks";
 
 function App() {
 
   const [isAppIitiated, setIsAppIitiated] = useState(false);
 
+  const instrumentState = useAppSelector(state=>state.instrument)
+  
   const navigate = useNavigate();
 
   const initializeApp = () => {
@@ -14,14 +18,19 @@ function App() {
   }
 
   useEffect(()=>{
-    isAppIitiated ? navigate(+1) : null;
+    isAppIitiated ? navigate('/instrument-selection') : null;
   },[isAppIitiated])
+
+  useEffect(()=>{
+    instrumentState && instrumentState.id != '' ? navigate('/instrument-display') : null;
+  },[instrumentState])
 
   return (
       <main className='transition-all ease min-h-screen flex flex-col items-center justify-start  bg-auto bg-repeat bg-zinc-900'>
         <Routes>
           <Route path="/" element={<AppStart onclick={initializeApp} additionalClasses={!isAppIitiated ? 'animate-drop-n-rotate' : 'animate-drop-n-rotate-reverse'} />} />
           <Route path="/instrument-selection" element={<WhatsTuning/>} />
+          <Route path="/instrument-display" element={<TunerArea/>} />
         </Routes>
       </main>
   )
---
+++ src/features/interests/TunerArea.tsx
@@ -0,0 +1,31 @@
+import { useAppSelector } from "../../hooks/hooks"
+import { useEffect } from "react" 
+
+import H1 from "../../components/headers/H1"
+import H2 from "../../components/headers/H2"
+
+const TunerArea = () => {
+
+  const instrumentState = useAppSelector(state=>state.instrument)
+
+  useEffect(()=>{
+    instrumentState.id != '' ? console.log(instrumentState) : console.log('nothing yet')
+  },[instrumentState])
+
+  return (
+    <section className='w-full text-white pb-8 md:pb-10 lg:pb-16 pt-0 text-center relative'>
+      
+      <div className="w-full sticky top-0 py-4 md:py-10 z-10 bg-zinc-800 border-b-[1px] border-zinc-800 after:absolute after:w-full after:h-full after:bg-white after:bg-opacity-0 after:backdrop-blur-md  after:-z-[2] bg-opacity-80 overflow-hidden after:left-0 after:!top-0">
+        <H1 additionalClasses={'!text-white'} headerText={`Lets Get Tuning  `} textIcon={undefined}/>
+        <H2 additionalClasses={'!text-emerald-500'} headerText={`Your ${`${instrumentState ? instrumentState.description.replace('-',' ') : null } ${instrumentState.title}`}`} textIcon={undefined}/>
+      </div>
+
+      <div className="w-full mx-auto md: md:w-11/12 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6 mt-8 px-8 md:px-10 lg:px-16">
+      
+      </div>
+    
+    </section>  
+  ) 
+}
+
+export default TunerArea
