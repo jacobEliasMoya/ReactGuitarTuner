e73f2a912f4f1f097907e4fe9bf4b47dc14063f9
Jake <jemoya1994@gmail.com>
2025-02-13 21:10:50 -0800

    more reactivity added lol .. messing with audio tomorrow - redbull and music time lol - after valentines day ofc

--- src/components/tuner/CalibrationDisplay.tsx
+++ src/components/tuner/CalibrationDisplay.tsx
@@ -4,7 +4,7 @@ type Props = {
 
 const CalibrationDisplay = (props: Props) => {
   return (
-        <div className="w-full mx-auto md:w-2/3 flex flex-row items-end justify-between gap-4 px-8 md:px-10 lg:px-16 relative">
+      <div className="w-full mx-auto md:w-2/3 flex flex-row items-end justify-between gap-4 relative">
 
         <div className="w-1 h-5 md:h-10 bg-zinc-600 rounded-full"></div>
         <div className="w-1 h-11 md:h-16 bg-zinc-600 rounded-full"></div>
@@ -18,10 +18,12 @@ const CalibrationDisplay = (props: Props) => {
         <div className="w-1 h-11 md:h-16 bg-zinc-600 rounded-full"></div>
         <div className="w-1 h-5 md:h-10 bg-zinc-600 rounded-full"></div>
 
-        <div className={`w-[1px] h-32 md:h-40 bg-red-600 rounded-full absolute bottom-0 z-20 left-[${props.calibrationNeedle ? props.calibrationNeedle : 0}px]`}></div>
-
-
-    </div>  
+        <div className="w-[1px] h-32 md:h-40 bg-red-600 rounded-full absolute bottom-0 z-20" 
+          style={{
+            left:props.calibrationNeedle
+          }}
+        ></div>
+      </div>  
   )
 }
 
--- src/features/interests/TunerArea.tsx
+++ src/features/interests/TunerArea.tsx
@@ -52,26 +52,31 @@ return (
   <section className='w-full min-h-screen text-white pt-0 text-center relative flex items-start justify-between flex-col'>
 
     <div className="w-full sticky top-0 py-4 md:py-10 z-10 bg-zinc-800 border-b-[1px] border-zinc-700 after:absolute after:w-full after:h-full after:bg-white after:bg-opacity-0 after:backdrop-blur-md  after:-z-[2] bg-opacity-80 overflow-hidden after:left-0 after:!top-0">
+
       <H1 additionalClasses={'!text-white'} headerText={`Lets Get Tuning  `} textIcon={undefined}/>
       <H2 additionalClasses={'!text-emerald-500'} headerText={`Your ${`${instrumentState ? instrumentState.description.replace('-',' ') : null } ${instrumentState.title}`}`} textIcon={undefined}/> 
+
     </div>
 
     <div className="w-full flex flex-col gap-20 items-end justify-center">
+
       <div className="w-full mx-auto md:w-11/12 flex items-center justify-center flex-col gap-4 md:gap-6  px-8 md:px-10 lg:px-16 md:mt-20 ">
-        <NoteDisplay note={"A#"} supportiveText={"Almost in Tune"} hurtz={0}/>
+        <NoteDisplay note={"A#"} supportiveText={"Almost in Tune"} hurtz={120}/>
       </div>
 
-      <CalibrationDisplay calibrationNeedle={15}/>
+      <CalibrationDisplay calibrationNeedle={122}/>
 
     </div>
 
     <div className="w-full flex flex-row items-center justify-center gap-4 md:gap-8 p-4 md:p-9 lg:p-11 bg-zinc-800 border-t-[1px] border-zinc-700 after:absolute after:w-full after:h-full after:bg-white after:bg-opacity-0 after:backdrop-blur-md  after:-z-[2] bg-opacity-80 overflow-hidden after:left-0 after:!top-0 md:mt-20 text-xs md:text-md lg:text-xl font-secondary ">
-      <div className="w-7 h-7 md:w-16 md:h-16 bg-zinc-500 border-2 border-zinc-600 rounded-full flex items-center justify-center">A</div>
-      <div className="w-7 h-7 md:w-16 md:h-16 bg-zinc-500 border-2 border-zinc-600 rounded-full flex items-center justify-center">B</div>
-      <div className="w-7 h-7 md:w-16 md:h-16 bg-zinc-500 border-2 border-zinc-600 rounded-full flex items-center justify-center">C</div>
-      <div className="w-7 h-7 md:w-16 md:h-16 bg-zinc-500 border-2 border-zinc-600 rounded-full flex items-center justify-center">D</div>
-      <div className="w-7 h-7 md:w-16 md:h-16 bg-zinc-500 border-2 border-zinc-600 rounded-full flex items-center justify-center">E</div>
-      <div className="w-7 h-7 md:w-16 md:h-16 bg-zinc-500 border-2 border-white rounded-full flex items-center justify-center">F</div>
+
+      <div className="w-7 h-7 md:w-16 md:h-16 bg-zinc-500 border-2 border-zinc-600 hover:border-white cursor-pointer rounded-full flex items-center justify-center">A</div>
+      <div className="w-7 h-7 md:w-16 md:h-16 bg-zinc-500 border-2 border-zinc-600 hover:border-white cursor-pointer rounded-full flex items-center justify-center">B</div>
+      <div className="w-7 h-7 md:w-16 md:h-16 bg-zinc-500 border-2 border-zinc-600 hover:border-white cursor-pointer rounded-full flex items-center justify-center">C</div>
+      <div className="w-7 h-7 md:w-16 md:h-16 bg-zinc-500 border-2 border-zinc-600 hover:border-white cursor-pointer rounded-full flex items-center justify-center">D</div>
+      <div className="w-7 h-7 md:w-16 md:h-16 bg-zinc-500 border-2 border-zinc-600 hover:border-white cursor-pointer rounded-full flex items-center justify-center">E</div>
+      <div className="w-7 h-7 md:w-16 md:h-16 bg-zinc-500 border-2 border-zinc-600 hover:border-white cursor-pointer rounded-full flex items-center justify-center">F</div>
+
     </div>
 
   </section>  
