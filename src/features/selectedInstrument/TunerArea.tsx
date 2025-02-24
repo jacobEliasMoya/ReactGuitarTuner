65b1a45e2abc095367422131e582d53bc28ceb05
Jake <jemoya1994@gmail.com>
2025-02-23 20:14:09 -0800

    just added a nested funciont ~if loop, want to try another method, and clean it up, but its pretty much working lol - needs a cleanup/ manana but a feasible minimum viable product ~ fancy fancee. Moving it upstairs to clean up

--- src/features/selectedInstrument/TunerArea.tsx
+++ src/features/selectedInstrument/TunerArea.tsx
@@ -15,6 +15,7 @@ const TunerArea = () => {
   const instrumentState = useAppSelector(state=>state.instrument); // my global state
 
   const [pitchHurtz, setPitchHurtz] = useState<number|null>(null)
+  const [hurtzDiffernce, sethurtzDiffernce] = useState<number|null>(null)
   const [currentNote, setCurrentNote] = useState<string>("");
   const [almostTuned, setAlmostTuned] = useState<string>("")
 
@@ -24,18 +25,41 @@ const TunerArea = () => {
 
   // ai taught me when it comes to music / audio streams classic for loops are the key ... lol me going straing to foreachs
   const approximateNote = (pitch:number) =>{
-    const offset:number = 25;
-    const midPoint:number = 15;
 
+    const offset:number = 25;
+    
     instrumentState.standardTuning.forEach((item)=>{
       if (pitch + offset > item.frequency &&   pitch - offset < item.frequency) {
         setAlmostTuned(`looks like your trying to tune ${item.note}`)
         setCurrentNote(`${item.note}`)
+        sethurtzDiffernce(pitch- item.frequency)
+
+        if (pitch + offset - 5 > item.frequency &&   pitch - offset - 5 < item.frequency) {
+          setAlmostTuned(`Getting Closer to  ${item.note}`)
+          setCurrentNote(`${item.note}`)
+          sethurtzDiffernce(pitch- item.frequency)
+
+          if (pitch + offset - 15 > item.frequency &&   pitch - offset - 15 < item.frequency) {
+            setAlmostTuned(`Super Close ${item.note}`)
+            setCurrentNote(`${item.note}`)
+            sethurtzDiffernce(pitch- item.frequency)
+            
+            if (pitch + offset - 21 > item.frequency &&   pitch - offset - 21 < item.frequency) {
+              setAlmostTuned(`Just a Hair off ${item.note}`)
+              setCurrentNote(`${item.note}`)
+              sethurtzDiffernce(pitch- item.frequency)
+
+              if (pitch + offset - 24 > item.frequency &&   pitch - offset - 24 < item.frequency) {
+                setAlmostTuned(`Pitch Perfect ${item.note} \n set to ${item.frequency}hz for perfection`)
+                setCurrentNote(`${item.note}`)
+                sethurtzDiffernce(pitch- item.frequency)
+              }
+            }
+          }
+        }
       }
     })
   }
-
-  
 
   const getMicStream = async () =>{
     try{
@@ -68,13 +92,12 @@ const TunerArea = () => {
 
       const audioContext = getAudioContext();
       const audioAnalyser = getAudioAnalyser(audioContext); 
+      // fun fact, higher fft = better low note detection ... just woah lol - audio crey 
 
-      // fun fact, higher fft = better low note detection ... just woah lol
       audioAnalyser.fftSize = 8192;
       const source = audioContext.createMediaStreamSource(micStream);
       const pitchDetect = detectPitch(audioContext.sampleRate);
       source.connect(audioAnalyser);
-
       audioContextRef.current = audioContext;
       audioAnalyserRef.current = audioAnalyser;
       dataBuffer.current = new Float32Array(audioAnalyser.fftSize);
@@ -90,14 +113,11 @@ const TunerArea = () => {
         }
         requestAnimationFrame(updateData);
       };
-
       updateData();
     }
-
     contextStreamConnect();
   },[])
 
- 
   return (
     <section className='w-full min-h-screen text-white pt-0 text-center relative flex items-start justify-between flex-col'>
 
@@ -109,7 +129,7 @@ const TunerArea = () => {
         <div className="w-full mx-auto md:w-11/12 flex items-center justify-center flex-col gap-4 md:gap-6  px-8 md:px-10 lg:px-16 md:mt-20  ">
           <NoteDisplay note={currentNote ? currentNote : ''} supportiveText={almostTuned ? almostTuned : "get plucking"} hurtz={pitchHurtz ? pitchHurtz.toFixed(2) : '0'}/>
         </div>
-        <CalibrationDisplay calibrationNeedle={0}/>
+        <CalibrationDisplay calibrationNeedle={hurtzDiffernce ? hurtzDiffernce : 0}/>
       </div>
       <div className="w-full flex flex-row justify-center lg:p-11 bg-zinc-800 border-t-[1px] border-zinc-700 after:absolute after:w-full after:h-full after:bg-white after:bg-opacity-0 after:backdrop-blur-md  after:-z-[2] bg-opacity-80 overflow-hidden after:left-0 after:!top-0 md:mt-20 text-xs md:text-md lg:text-xl font-secondary ">
         <div className="w-full lg:w-1/2 flex flex-row justify-center lg:rounded-3xl overflow-hidden border-4 border-t-zinc-700 border-r-zinc-700 border-b-zinc-900 border-zinc-900">
